d56fa8116e6229342ef4e67b8d3a7bbbc8605038
Vinicius <148067296+v1niisx@users.noreply.github.com>
2025-07-01 20:10:49 +0000

    Atualizando código do Streamlit

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from supabase import create_client, Client # type: ignore
 
 # Substitua com sua URL e API Key do Supabase
 url = "https://lhawtmsyqpvghxyzlvep.supabase.co"
-key = ""
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoYXd0bXN5cXB2Z2h4eXpsdmVwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NjQ3OTYxMCwiZXhwIjoyMDYyMDU1NjEwfQ.pX5_3QYLNJayOKVD_yxJY_B0IWxFbalFXOpqMXTyb8A"
 supabase: Client = create_client(url, key)
